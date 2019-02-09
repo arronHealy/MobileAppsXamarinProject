6c2917fe0ed15c3d2133e86be3aaa6618436c570
arronHealy <G00346883@gmit.ie>
2019-02-09 16:58:37 +0000

    SQLite added & note, contacts models added

---
+++ MobileAppsDev2_XamarinProject/MobileAppsDev2_XamarinProject/Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+
+namespace MobileAppsDev2_XamarinProject.Models
+{
+    class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public string phoneNumber { get; set; }
+
+        public string email { get; set; }
+
+        public string address { get; set; }
+
+        public string company { get; set; }
+
+        public Contact()
+        {
+
+        }//Note
+    }
+}
---
+++ MobileAppsDev2_XamarinProject/MobileAppsDev2_XamarinProject/Models/Note.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace MobileAppsDev2_XamarinProject.Models
+{
+    class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string title { get; set; }
+
+        public string note { get; set; }
+
+        public Note()
+        {
+
+        }//Note
+    }
+}
